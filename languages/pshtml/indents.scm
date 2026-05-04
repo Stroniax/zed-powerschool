a98d268b79689a619948ba5721dc26a69c9f635c
Caleb Frederickson <caleb.frederickson@gmail.com>
2026-05-04 15:50:22 -0800

    add indents

---
+++ languages/pshtml/indents.scm
@@ -0,0 +1,21 @@
+(start_tag
+  ">" @end) @indent
+
+(self_closing_tag
+  "/>" @end) @indent
+
+(element
+  (start_tag) @start
+  (end_tag)? @end) @indent
+
+(ps_if
+    (ps_if_tag) @start
+    (ps_if_end_tag) @end
+    ) @indent
+
+(tlist_sql
+    "tlist_sql" @start
+    "tlist_sql" @end ) @indent
+
+(tlist_query) @indent
+(tlist_template) @indent
