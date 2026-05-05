8ad53457794ee1e1fb6c73bedd90916bfeb9eb07
Caleb Frederickson <caleb.frederickson@gmail.com>
2026-05-05 08:09:41 -0800

    improve tlist_sql indentation

--- languages/pshtml/indents.scm
+++ languages/pshtml/indents.scm
@@ -10,12 +10,12 @@
 
 (ps_if
     (ps_if_tag) @start
-    (ps_if_end_tag) @end
+    (ps_if_end_tag)? @end
     ) @indent
 
-(tlist_sql
-    "tlist_sql" @start
-    "tlist_sql" @end ) @indent
-
 (tlist_query) @indent
-(tlist_template) @indent
+
+(tlist_sql
+    "]" @start
+    "tlist_sql"? @end
+) @indent
